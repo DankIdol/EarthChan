7166e228325d0f83b5a2573ddeeffa0b691ac2c5
RazorSh4rk <iamrazorshark@gmail.com>
2018-08-11 20:08:39 +0200

    despacito

--- src/states/Game.js
+++ src/states/Game.js
@@ -13,7 +13,10 @@ var w = 1280/16,
 	listeners = new Listeners(),
 	popup,
 	selectedTile,
-	btnGroup
+	btnGroup,
+	arrowGroup,
+	selectedArrow,
+	showArrows = false
 /** */
 
 
@@ -25,28 +28,52 @@ export default class extends Phaser.State {
   	selectedTile = tile.myId
   	tiles.forEach(e => e.forEach(f => f.inputEnabled = false))
   	btnGroup.children.forEach(e => e.scale.set(1))
-  	console.log(selectedTile)
+  	tiles[selectedTile[0]][selectedTile[1]].tint = 0x00ffff
   }
   
   closeClickListener(tile, evt){
+  	showArrows = false
+  	tiles[selectedTile[0]][selectedTile[1]].tint = 0xff8000
   	selectedTile = ''
   	tiles.forEach(e => e.forEach(f => f.inputEnabled = true))
   	btnGroup.children.forEach(e => e.scale.set(0))
   }
 
   buttonClickListener(button){
+	showArrows = true
   	btnGroup.children.forEach(e => e.scale.set(0))
+  	arrowGroup.children.forEach(e => e.scale.set(0.5))
   }
   buttonUpListener(button){
+  	showArrows = false
   	btnGroup.children.forEach(e => e.scale.set(1))
+  	arrowGroup.children.forEach(e => e.scale.set(0))
   }
-  
+  arrowSelectListener(arrow){
+  	if(showArrows){
+  		arrow.scale.set(0.6)
+  		selectedArrow = arrow.facing
+  	}
+  }
+  arrowDeSelectListener(arrow){
+	if(showArrows){
+    	arrow.scale.set(0.5)
+    	selectedArrow = null
+    }
+  }
+  arrowTriggerSelectListener(arrow){
+      if(selectedArrow != null){
+      	console.log(selectedArrow)
+      }
+  }
+    
   preload() { 
 	this.game.load.image('map', '../../assets/images/earth_map.jpg')
 	this.game.load.image('boundingBox', '../../assets/images/bound.png')
 	this.game.load.image('popupBox', '../../assets/images/popup.png')
 	this.game.load.image('button', '../../assets/images/button_small.png')
 	this.game.load.image('close', '../../assets/images/close.png')
+	this.game.load.image('arrow', '../../assets/images/cursor.png')
 	data.earthEvents.forEach(e => {
 		this.game.load.image(e.name, '../../assets/images/' + e.sprite)
 	})
@@ -55,7 +82,7 @@ export default class extends Phaser.State {
 
   create() {
 	var background = this.game.add.tileSprite(0, 0, 1280, 720, 'map')
-
+	var fontStyle = { font: '15px monospace'}
 	
 	for(let i = 0; i < 9; i++){ tiles.push([]) }
 	
@@ -71,8 +98,8 @@ export default class extends Phaser.State {
 	btnGroup = game.add.group()
 
 	let i = 0,
-		offsetY1 = -135,
-		offsetY2 = -135
+		offsetY1 = -140,
+		offsetY2 = -140
 
 	let closeBtn = this.game.add.sprite((1280 - 50), 50, 'close')
 	closeBtn.anchor.set(0.5)
@@ -87,7 +114,9 @@ export default class extends Phaser.State {
 			btn.inputEnabled = true
 			btn.events.onInputDown.add(this.buttonClickListener, this)
 			btn.events.onInputUp.add(this.buttonUpListener, this)
-			let tx = this.game.add.text(game.world.centerX - 260, game.world.centerY + (offsetY1 - 30), e.name, 5)
+			let tx = this.game.add.text(game.world.centerX - 260, game.world.centerY + (offsetY1 - 25), 
+				e.name + '\nCD: ' + (e.cooldown/60000).toFixed(1) + 
+				'm HE: ' + e.humanCost + ' BE: ' + e.buildingCost, fontStyle)
 			btn.anchor.set(0.5)
 			offsetY1 += 90
 			btnGroup.add(btn)			
@@ -98,7 +127,9 @@ export default class extends Phaser.State {
 			btn.inputEnabled = true
 			btn.events.onInputDown.add(this.buttonClickListener, this)
 			btn.events.onInputUp.add(this.buttonUpListener, this)
-			let tx = this.game.add.text(game.world.centerX + 40, game.world.centerY + (offsetY2 - 30), e.name)
+			let tx = this.game.add.text(game.world.centerX + 30, game.world.centerY + (offsetY2 - 30),
+				e.name + '\nCD: ' + (e.cooldown/60000).toFixed(1) + 
+				'm HE: ' + e.humanCost + ' BE: ' + e.buildingCost, fontStyle)
 			btn.anchor.set(0.5)
 			offsetY2 += 90
 			btnGroup.add(btn)
@@ -107,6 +138,34 @@ export default class extends Phaser.State {
 		i++
 	})
 
+	arrowGroup = game.add.group()
+	let arrowUp = this.game.add.button(game.world.centerX, game.world.centerY - 200, 'arrow', null, this,2,1,0)
+	arrowUp.facing = 0
+	
+	let arrowDown = this.game.add.button(game.world.centerX, game.world.centerY + 200, 'arrow', null, this,2,1,0)
+	arrowDown.angle += 180
+	arrowDown.facing = 1
+	
+	let arrowLeft = this.game.add.button(game.world.centerX - 400, game.world.centerY, 'arrow', null, this,2,1,0)
+	arrowLeft.angle -= 90
+	arrowLeft.facing = 2
+	
+	let arrowRight = this.game.add.button(game.world.centerX + 400, game.world.centerY, 'arrow', null, this,2,1,0)
+	arrowRight.angle += 90
+	arrowRight.facing = 3
+	
+	arrowGroup.add(arrowUp)
+	arrowGroup.add(arrowDown)
+	arrowGroup.add(arrowLeft)
+	arrowGroup.add(arrowRight)
+	arrowGroup.children.forEach(e => {
+		e.anchor.set(0.5)
+		e.scale.set(0)
+		e.events.onInputOver.add(this.arrowSelectListener, this)
+		e.events.onInputOut.add(this.arrowDeSelectListener, this)
+		e.events.onInputUp.add(this.arrowTriggerSelectListener, this)
+	})
+	
     /*this.mushroom = new Mushroom({
       game: this.game,
       x: this.world.centerX,
